2dbc585a64d2397556f54226733bd7ac7a0237ae
albannablade123 <hasanal082000@gmail.com>
2024-06-10 19:45:07 +0100

    - chore: Added more test cases

--- product-service/src/test/java/com/hasan/productservice/ProductServiceTest.java
+++ product-service/src/test/java/com/hasan/productservice/ProductServiceTest.java
@@ -1,9 +1,13 @@
 package com.hasan.productservice;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +19,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.hasan.productservice.Entity.Product;
 import com.hasan.productservice.Entity.Product.ProductBuilder;
@@ -69,4 +75,53 @@ public class ProductServiceTest {
 
         
     }
+
+    @Test
+    public void ProductService_GetAllProduct_ReturnsResponseDto(){
+        Page<Product> products = Mockito.mock(Page.class);
+
+        when(productRepository.findAll(Mockito.any(Pageable.class))).thenReturn(products);
+
+        List<ProductDto> saveProduct = productService.getProducts(1,10);
+
+        Assertions.assertThat(saveProduct).isNotNull();
+
+    }
+
+    @Test
+    public void ProductService_FindById_ReturnProductDto() {
+        Long productId = 1L;
+        Product product = Product.builder()
+            .id(1L)
+            .name("test product")
+            .description("test description")
+            .price(1000L)
+            .images(Arrays.asList("image1.jpg", "image2.jpg"))
+            .createdDate(LocalDateTime.now())
+            .build();
+        when(productRepository.findById(productId)).thenReturn(Optional.ofNullable(product));
+
+        Product productReturn = productService.getProduct(productId);
+
+        Assertions.assertThat(productReturn).isNotNull();
+    }
+
+    @Test
+    public void ProductService_DeleteProductById_ReturnVoid() {
+        Long productId = 1L;
+        Product product = Product.builder()
+            .id(1L)
+            .name("test product")
+            .description("test description")
+            .price(1000L)
+            .images(Arrays.asList("image1.jpg", "image2.jpg"))
+            .createdDate(LocalDateTime.now())
+            .build();
+
+        when(productRepository.findById(productId)).thenReturn(Optional.ofNullable(product));
+        doNothing().when(productRepository).delete(product);
+
+        assertAll(() -> productService.deleteProduct(productId));
+    }
+
 }
